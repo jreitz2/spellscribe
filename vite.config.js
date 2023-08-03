d12c3d2a6ac0bfb344257cae964892cfe1e64ebc
jreitz2 <jreitz2@gmail.com>
2023-08-03 14:48:43 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/spellscribe/",
+  base: "/https://spellscribe.onrender.com/",
   plugins: [react()],
 })
